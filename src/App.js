c3fb971ad9372d6940b7726051f7ecb4648fc4bb
rohitpatilweb <74045753+rohitpatilweb@users.noreply.github.com>
2021-12-11 22:02:34 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import FronPage from "./Components/FrontPage";
-import { View } from "react-native";
-
+import { Text, View } from "react-native";
 
 function App() {
   return (
     <View>
-     <FronPage/>
+      <Text> sdf</Text>
+      <FronPage />
     </View>
   );
 }
