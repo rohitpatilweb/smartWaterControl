9c3cb2029a959278833609a8ead8e8f4895787c6
rohitpatilweb <74045753+rohitpatilweb@users.noreply.github.com>
2021-12-12 10:49:55 +0530

    frontpage

--- src/Components/FrontPage.js
+++ src/Components/FrontPage.js
@@ -1,100 +1,101 @@
 import LogInPage from ".././Components/LogInPage";
 import SignUpPage from ".././Components/SignUpPage";
-
 import {
   TouchableOpacity,
   ImageBackground,
   StyleSheet,
   Text,
   View,
+  Navigator,
   onPress
 } from "react-native";
+import FadeInView from "./fadein";
 const signInHandler = () => {};
 const logInHandler = () => {};
-
 
 function FrontPage() {
   return (
     <view>
       <ImageBackground
-        source={require("../Assets/Drop.jpeg")}
+        source={require("../Assets/4.jpg")}
         resizeMode="cover"
         style={styles.image}
       >
-        <View style={styles.ok}>
-          <Text style={styles.font}>
-            You are one step away from saving water !
-          </Text>
-        </View>
+        <FadeInView>
+          <View style={styles.container}>
+            <View style={styles.textBox}>
+              <Text style={styles.font}>
+                You are one step away from saving water !
+              </Text>
+            </View>
 
-        <View style={styles.fixToText}>
-          <TouchableOpacity style={styles.button} onPress={logInHandler}>
-            <Text style={styles.buttonText}>L O G   I N</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={signInHandler}>
-            <Text style={styles.buttonText}>S I G N   I N</Text>
-          </TouchableOpacity>
-        </View>
+            <View style={styles.fixToText}>
+              <TouchableOpacity style={styles.button} onPress={logInHandler}>
+                <Text style={styles.buttonText}>L O G I N</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={signInHandler}>
+                <Text style={styles.buttonText}>S I G N U P</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </FadeInView>
       </ImageBackground>
     </view>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    height: "100vh",
+    width: "100%",
+    justifyContent: "space-between"
+  },
   image: {
     height: "100vh",
     width: "100%"
   },
-  ok: {
-    // shadowColor: "rgba(0, 0, 0, 1)",
-    // shadowOffset: { width: -12, height: 12 },
-    // shadowOpacity: 0.4,
-    // shadowRadius: 9,
+  textBox: {
     justifyContent: "center",
     textAlign: "center",
     marginTop: 52,
-    marginLeft: "15%",
+    marginLeft: "10%",
     height: "auto",
-    width: "70%",
+    width: "80%",
     padding: 12,
-    borderRadius: 12,
+    borderRadius: 12
   },
   font: {
-   textShadowColor: "rgba(0, 0, 0, 0.9)",
-   textShadowOffset: {width: -1, height: 1},
-   textShadowRadius: 10,
+    textShadowColor: "rgba(0, 0, 0, 0.5)",
+    textShadowOffset: { width: -5, height: 5 },
+    textShadowRadius: 6,
     fontWeight: "bold",
-    fontSize: 27,
-    color:"#808080",
-    // color: "rgba(150, 100, 100, 150)"
+    fontSize: 32,
+    color: "#010014"
   },
   fixToText: {
     flexDirection: "column",
     justifyContent: "space-evenly",
-    marginTop: "100%"
+    marginBottom: "5%"
   },
 
   button: {
-    shadowColor: "rgba(0, 0, 0, 1)",
-    shadowOffset: { width: -5, height: 6 },
-    shadowOpacity: 0.4,
+    shadowColor: "#7fffd4",
+    shadowOffset: { width: -4, height: 4 },
+    shadowOpacity: 0.29,
     shadowRadius: 9,
     marginLeft: "7%",
     marginBottom: "5%",
-    height: 50,
+    height: 45,
     width: "86%",
-    // backgroundColor: "black",
-    padding: 15,
-    borderRadius: 10
-    //opacity: 0.6,
+    backgroundColor: "#000000",
+    borderColor: "#7fffd4",
+    // borderWidth: ,
+    padding: 12,
+    borderRadius: 12
   },
   buttonText: {
-    textShadowColor: "rgba(0, 0, 0, 0.9)",
-    textShadowOffset: {width: -8, height: 8},
-    textShadowRadius: 10,
-    color: "black",
-    fontSize: 20,
-    fontWeight: "bold",
+    color: "#90ee90",
+    fontSize: 15,
     alignSelf: "center"
   }
 });
